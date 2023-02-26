0bb01c0ab5751b2cc1cee7da51f167e707104359
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-02-26 09:15:20 -0800

    added basic styling + installed react icon lib

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,11 @@ import { CiDark } from "react-icons/ci";
 
 function Header() {
   return (
-    <div>
+    <div className="flex items-center justify-between p-5 bg-orange-700 h-20 sticky border-b border-b-black">
       <h1>Where in the world?</h1>
-      <div>
-              <CiDark />
-              <h3>Dark Mode</h3>
+      <div className="flex items-center">
+        <CiDark />
+        <h3 className="pl-2">Dark Mode</h3>
       </div>
     </div>
   );
