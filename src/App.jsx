7caf069fb513d274a0c41cbb292fcb8f66181254
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-02-26 08:33:12 -0800

    installed axios and created the api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends Component {
+  state = {
+    countries: null,
+  };
+
+  getData = () => {
+    axios
+      .get("http://localhost:8080")
+      .then((res) =>
+        this.setState(
+          {
+            countries: res.data,
+          },
+          // console.log(res.data)
+        )
+      )
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    console.log(this.state.countries)
+    return <div>App</div>;
+  }
 }
-
-export default App;
