a3dd1d6cd3a3345f88663e6d875c4cfcd8a9e567
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-03-05 21:53:57 -0800

    added filter functionality

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 
 function Main(countries) {
+  const [search, setSearch] = useState("");
+
+  const changeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="p-5">
       <div>
-        <input type="text" placeholder="Search for a country..." />
+        <form action="submit" onChange={changeHandler}>
+          <input type="text" placeholder="Search for a country..." />
+        </form>
         <Menu menuButton={<MenuButton>Filter by Region</MenuButton>}>
           <MenuItem>Africa</MenuItem>
           <MenuItem>America</MenuItem>
@@ -16,27 +24,33 @@ function Main(countries) {
         </Menu>
       </div>
       <div>
-        {countries.countries.map((country, index) => {
-          return (
-            <Link to={country.name} key={index}>
-              <div className="my-5 pb-16 border border-grey-600 shadow-lg rounded-lg bg-white w-3/4">
-                <img src={country.flag} alt={country.name} />
-                <div className="pl-5">
-                  <h4 className="font-bold py-5">{country.name}</h4>
-                  <h4 className='before:content-["Population:"] before:font-medium before:pr-1'>
-                    {country.population}
-                  </h4>
-                  <h4 className='before:content-["Region:"] before:font-semibold before:pr-1'>
-                    {country.region}
-                  </h4>
-                  <h4 className='before:content-["Capital:"] before:font-semibold before:pr-1'>
-                    {country.capital}
-                  </h4>
+        {countries.countries
+          .filter((item) => {
+            return search.toLowerCase() === ""
+              ? item
+              : item.name.toLowerCase().includes(search);
+          })
+          .map((country, index) => {
+            return (
+              <Link to={country.name} key={index}>
+                <div className="my-5 pb-16 border border-grey-600 shadow-lg rounded-lg bg-white w-3/4">
+                  <img src={country.flag} alt={country.name} />
+                  <div className="pl-5">
+                    <h4 className="font-bold py-5">{country.name}</h4>
+                    <h4 className='before:content-["Population:"] before:font-medium before:pr-1'>
+                      {country.population}
+                    </h4>
+                    <h4 className='before:content-["Region:"] before:font-semibold before:pr-1'>
+                      {country.region}
+                    </h4>
+                    <h4 className='before:content-["Capital:"] before:font-semibold before:pr-1'>
+                      {country.capital}
+                    </h4>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
